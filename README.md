5ba0551f62a22960f24e74c1ea086d4afc7862e3
Dhruv <51574716+Illusion47586@users.noreply.github.com>
2023-01-18 12:54:44 +0530

    fix(keygen): added content key generator

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ You need a `.env.development.local` for local dev setup and a `.env.production.l
 |Key|Expected value|
 |---|--------------|
 |DATABASE_URL|URL of the `MySQL` database|
-|CONTENT_KEY|16 Character Key containing only characters and numbers (generate [here](https://passwordsgenerator.net/?length=16&symbols=0&numbers=1&lowercase=0&uppercase=1&similar=0&ambiguous=0&client=1&autoselect=0))|
+|CONTENT_KEY|16 Character Key containing only characters and numbers (generate using `pnpm keygen:dev`)|
 |NEXT_PUBLIC_CONTENT_KEY|Same as above|
 |NEXT_PUBLIC_URL|URL where this instance is running|
 
---
+++ generator.ts
@@ -0,0 +1,56 @@
+import Cryptr from 'cryptr';
+import RandomString from 'randomstring';
+
+const frontCryptr = new Cryptr(process.env.NEXT_PUBLIC_CONTENT_KEY!);
+const serverCryptr = new Cryptr(process.env.CONTENT_KEY!);
+
+const getRandomText = () =>
+    RandomString.generate({
+        length: 5,
+        readable: true,
+        capitalization: 'lowercase',
+        charset: 'alphabetic',
+    });
+
+const generateCryptoVersion = (key = getRandomText()) => {
+    console.log('KEY');
+    console.log(key);
+    console.log();
+    const fe = frontCryptr.encrypt(key);
+    console.log('FRONTEND ONLY CRYPT');
+    console.log(fe);
+    console.log();
+    const se = serverCryptr.encrypt(key);
+    console.log('SERVER ONLY CRYPT');
+    console.log(se);
+    console.log();
+    const fse = serverCryptr.encrypt(fe);
+    console.log('FRONTEND + SERVER CRYPT');
+    console.log(fse);
+};
+
+const keygen = () => {
+    const FE_CONTENT_KEY = RandomString.generate({
+        length: 16,
+        capitalization: 'uppercase',
+        charset: 'alphanumeric',
+    });
+
+    const BE_CONTENT_KEY = RandomString.generate({
+        length: 16,
+        capitalization: 'uppercase',
+        charset: 'alphanumeric',
+    });
+
+    console.log('HERE ARE RANDOMLY GENERATED KEYS');
+    console.table({
+        FE_CONTENT_KEY: { 'ENV VAR': 'NEXT_PUBLIC_CONTENT_KEY', KEY: FE_CONTENT_KEY },
+        BE_CONTENT_KEY: { 'ENV VAR': 'CONTENT_KEY', KEY: BE_CONTENT_KEY },
+    });
+};
+
+if (process.argv[2] === 'keygen') keygen();
+else if (process.argv[2] === 'cryptogen') {
+    if (process.argv[3]) generateCryptoVersion(process.argv[3]);
+    else generateCryptoVersion();
+}
